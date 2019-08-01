85b45684afa806be1a0d6e050c8fae208c2b6eaf
Saurabh22415 <saurabh.ranjan50@gmail.com>
2019-08-01 15:17:22 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,13 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { UserComponent } from './users/user/user.component';
 import { ServersService } from './servers/servers.service';
+import {Routes} from '@angular/router'
 
+const appRoutes : Routes = [
+    {path :'', component : HomeComponent},
+    {path :'user', component : UserComponent},
+    {path :'server', component : ServerComponent}
+]
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
   declarations: [ AppComponent, HelloComponent, HomeComponent, ServersComponent, UsersComponent, EditServerComponent, ServerComponent, UserComponent ],
